2586a653eb73d9aa7e7a5fdcf5c1e22006e0164f
Isaac Warren <isaacwarren25@gmail.com>
2024-02-07 11:14:27 -0500

    Edited countKeywords method to ignore String, line comments, and block comments.

--- src/CountKeywords.java
+++ src/CountKeywords.java
@@ -1,6 +1,11 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * CountKeywords takes a command line argument of a java file and counts the java keywords.
+ * It ignores keywords found in Strings, line comments, and block comments.
+ * @author Isaac Warren
+ */
 public class CountKeywords {
     public static void main(String[] args) throws Exception {
         // Check for command line argument
@@ -9,9 +14,7 @@ public class CountKeywords {
             System.exit(1);
         }
 
-
-
-
+        // Check for file and display count of keywords
         File file = new File(args[0]);
         if (file.exists()) {
             System.out.println("The number of keywords in " + args[0]
@@ -22,6 +25,8 @@ public class CountKeywords {
         }
     }
 
+    /** countKeywords takes a File object and counts java keywords within
+     * while ignoring Strings, comments and block comments*/
     public static int countKeywords(File file) throws Exception {
         // Array of all Java keywords + true, false and null
         String[] keywordString = {"abstract", "assert", "boolean",
@@ -35,6 +40,7 @@ public class CountKeywords {
                 "throw", "throws", "transient", "try", "void", "volatile",
                 "while", "true", "false", "null"};
 
+        // creates HashSet for efficiency
         Set<String> keywordSet =
                 new HashSet<>(Arrays.asList(keywordString));
         int count = 0;
@@ -43,17 +49,25 @@ public class CountKeywords {
 
         while (input.hasNext()) {
             String word = input.next();
-            // TODO COMMENT if "//"" don't count
-            if (){
 
-            }
-            // TODO STRING elseif "\"" don't count while not "\"
-            // TODO BLOCK COMMENT else if "/*" don't count while not "*/"
+            if (word.equals("//")){// ignores line comment
+                input.nextLine();
+            } else if (word.equals("\"")) {// starts ignore of String
+                String nextWord;
+                do {
+                    nextWord = input.next();
+                } while (!nextWord.contains("\"")); //ends ignore of String
 
-            if (keywordSet.contains(word))
+            } else if (word.contains("/*")) {   // start ignore of block comment
+                String nextWord;
+                do {
+                    nextWord = input.next();
+                } while (!nextWord.contains("*/")); //end ignore of block comment
+
+            } else if (keywordSet.contains(word)) { // count keyword
                 count++;
+            }
         }
-
         return count;
     }
 }
